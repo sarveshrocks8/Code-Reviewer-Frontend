2915b83a1e832d27f2a82ebefd94b529eff1fb35
Sarvesh Chaudhary <sc418760@gmail.com>
2025-06-23 22:55:48 +0530

    api base url added

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-const API_BASE_URL = "http://localhost:3000"; // Backend ka base URL
+const API_BASE_URL = "https://code-reviewer-backend-beta.vercel.app/"; // Backend ka base URL
 export default API_BASE_URL;
