9beb577310a533c59bbd7425922e3520d0e91e21
Sarvesh Chaudhary <sc418760@gmail.com>
2025-06-28 14:57:40 +0530

    little change

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,7 +13,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/api/auth/login", {
+      const response = await fetch("https://code-reviewer-backend-beta.vercel.app/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
